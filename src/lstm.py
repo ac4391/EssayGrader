116951bd031b19aae156585ceabb84db5d87d889
Ariel Cohen Codar <ac4391@columbia.edu>
2018-11-28 14:05:38 -0500

    lstm changes

--- src/lstm.py
+++ src/lstm.py
@@ -7,14 +7,14 @@ import time
 import os
 
 class LSTM_RNN():
-    def __init__(self, num_classes, batch_size=64, num_steps=50, cell_type='LSTM',
+    def __init__(self, num_classes, batch_size=64, num_steps=500, cell_type='LSTM',
                  rnn_size=128, num_layers=2, learning_rate=0.001, train_keep_prob=0.5, sampling=False):
         '''
         Initialize the input parameter to define the network
         inputs:
         :param num_classes: (int) the vocabulary size of your input data
         :param batch_size: (int) number of sequences in one batch
-        :param num_steps: (int) length of each seuqence in one batch
+        :param num_steps: (int) length of each sequence in one batch
         :param cell_type: your rnn cell type, 'LSTM' or 'GRU'
         :param rnn_size: (int) number of units in one rnn layer
         :param num_layers: (int) number of rnn layers
@@ -42,15 +42,12 @@ class LSTM_RNN():
         self.my_optimizer()
         self.saver = tf.train.Saver()
         
-def inputs_layer(self):
+    def inputs_layer(self):
 
         self.inputs = tf.placeholder(tf.int32, shape=(self.batch_size, self.num_steps), name='inputs')
-        # add keep_prob
-        self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
-        # one_hot encoding
-        self.rnn_inputs = tf.one_hot(self.inputs, self.num_classes)
+        self.targets = tf.placeholder(tf.int32, shape=(self.batch_size, 1), name='targets')
         
-def rnn_layer(self):
+    def rnn_layer(self):
         
         if (cell_type == 'LSTM'):
             cell = LSTMCell(self.rnn_size)
@@ -61,11 +58,11 @@ def rnn_layer(self):
         self.initial_state = cell.zero_state(self.batch_size, dtype = tf.float32)
         
         
-        self.rnn_outputs, self.final_state = tf.nn.dynamic_rnn(cell, rnn_inputs, initial_state = self.initial_state, dtype=tf.float32)
+        self.rnn_outputs, self.final_state = tf.nn.dynamic_rnn(cell, self.inputs, initial_state = self.initial_state, dtype=tf.float32)
         
-def outputs_layer(self):
+    def outputs_layer(self):
 
-        seq_output = tf.concat(self.rnn_outputs, axis=1)
+        seq_output = self.rnn_outputs #Perhaps we need to transpose this
         x = tf.reshape(seq_output, [-1, self.rnn_size])
         
         # define softmax layer variables:
@@ -79,13 +76,12 @@ def outputs_layer(self):
         # softmax generate probability predictions
         self.prob_pred = tf.nn.softmax(self.logits, name='predictions')
         
-def my_loss(self):
-
-        # One-hot coding
-        y_one_hot = tf.one_hot(self.targets, self.num_classes)
-        y_reshaped = tf.reshape(y_one_hot, self.logits.get_shape())
+    def my_loss(self):
+    
+        y_reshaped = tf.reshape(self.targets, self.logits.get_shape())
         
         # Softmax cross entropy loss
         loss = tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=y_reshaped)
         self.loss = tf.reduce_mean(loss)     
         self.optimizer = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
+        
