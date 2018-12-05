c352530510daf82919cda986b913738f168c98b5
Marko Mandic <Marko@dyn-209-2-219-161.dyn.columbia.edu>
2018-12-05 13:23:14 -0800

    removed a layer from mlp, changed hyperparameters, added saver, prediction function, changed flatten to average, added scraped qwk function

--- src/mlp.py
+++ src/mlp.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-
+from src.utils import normalize_predictions
 
 class MLP(object):
 
@@ -22,39 +22,56 @@ class MLP(object):
 
         self.W1 = tf.Variable(1e-2 * np.random.rand(self.input_dim, self.hidden_dims[0]).astype('float32'))
         self.b1 = tf.Variable(np.zeros((self.hidden_dims[0],)).astype('float32'))
-        self.W2 = tf.Variable(1e-2 * np.random.rand(self.hidden_dims[0], self.hidden_dims[1]).astype('float32'))
-        self.b2 = tf.Variable(np.zeros((self.hidden_dims[1],)).astype('float32'))
-        self.W3 = tf.Variable(1e-2 * np.random.rand(self.hidden_dims[1], self.output_dim).astype('float32'))
-        self.b3 = tf.Variable(np.zeros((self.output_dim,)).astype('float32'))
+        self.W2 = tf.Variable(1e-2 * np.random.rand(self.hidden_dims[0], self.output_dim).astype('float32'))
+        self.b2 = tf.Variable(np.zeros((self.output_dim,)).astype('float32'))
 
-        h1_tf = tf.nn.sigmoid(tf.matmul(self.X, self.W1) + self.b1)
-        h2_tf = tf.nn.relu(tf.matmul(h1_tf, self.W2) + self.b2)
-        h3_tf = tf.matmul(h2_tf, self.W3) + self.b3
+        h1_tf = tf.nn.relu(tf.matmul(self.X, self.W1) + self.b1)
+        h2_tf = tf.matmul(h1_tf, self.W2) + self.b2
 
 
-        cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=h3_tf, labels=tf.one_hot(self.y, num_classes))
-        L2_loss = tf.nn.l2_loss(self.W1) + tf.nn.l2_loss(self.W2) + tf.nn.l2_loss(self.W3)
+        cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=h2_tf, labels=tf.one_hot(self.y, num_classes))
+        L2_loss = tf.nn.l2_loss(self.W1) + tf.nn.l2_loss(self.W2) + tf.nn.l2_loss(self.W2)
         self.loss = tf.reduce_mean(cross_entropy) + l2_reg*L2_loss
 
-        self.preds = tf.argmax(h3_tf,1)
+        self.preds = tf.argmax(h2_tf,1)
         correct_prediction = tf.equal(self.y, self.preds)
         self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        self.saver = tf.train.Saver()
 
     def train(self, gen, X_val, y_val, n_epochs, lr):
 
-
-        self.train_step = tf.train.GradientDescentOptimizer(lr).minimize(self.loss)
-
+        self.train_step = tf.train.AdamOptimizer(lr).minimize(self.loss)
+        best_acc = 0
         init = tf.global_variables_initializer()
         with tf.Session() as sess:
+            saver = tf.train.Saver()
             sess.run(init)
             counter = 0
             for batch_X, batch_y in gen:
                 counter += 1
                 loss, _ = sess.run([self.loss,self.train_step], feed_dict={self.X: batch_X, self.y: batch_y})
-                if counter % 5 ==0:
+                if counter % 7000 == 0:
                     preds,vals, val_acc = sess.run([self.preds,self.y, self.accuracy], feed_dict={self.X: X_val, self.y: y_val})
                     print("loss for counter {} is {}".format(counter, loss))
                     print('counter {}: valid acc = {}'.format(counter, val_acc))
                     print(preds[:20])
                     print(vals[:20])
+                    if val_acc > best_acc:
+                        print('Best validation accuracy! iteration:{} accuracy: {}%'.format(counter, val_acc))
+                        best_acc = val_acc
+                        self.saver.save(sess, 'model/{}'.format(counter))
+                    if counter > n_epochs * 7000:
+                        break
+    
+    def predict(self, checkpoint, X_test, X_set):
+        
+        self.session = tf.Session()
+        with self.session as sess:
+            predictions = tf.zeros(shape = X_test.shape)
+            self.saver.restore(sess, checkpoint)
+            for i, x in enumerate(X_test):
+                preds = sess.run(self.preds, feed_dict={self.X: x})
+                norm_pred = normalize_predictions(preds[0], X_set[i])
+                predictions[i] = norm_pred
+        return predictions
+    
